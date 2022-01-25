83d0c7df7de1261c69fe07014e3ccee8e6c6219f
Sergio Arias Ruda <sergiariasruda@gmail.com>
2022-01-25 19:06:54 +0100

    Maven container testing

--- src/main/java/es/us/isa/ideas/app/configuration/StudioConfigurationInitializator.java
+++ src/main/java/es/us/isa/ideas/app/configuration/StudioConfigurationInitializator.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.web.context.support.ServletContextAttributeExporter;
 
 /**
@@ -20,6 +21,7 @@ import org.springframework.web.context.support.ServletContextAttributeExporter;
 @Configuration
 public class StudioConfigurationInitializator {
 
+    @Lazy
     @Autowired
     StudioConfiguration value;
     
